5138116614224ec3bb443c398fefed0e0bf4af62
Sumit Kumar Jha <jhasumit4742@gmail.com>
2023-12-28 21:24:29 +0530

    Created the profile view page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Preview from "./pages/Preview";
+
+function App() {
+  return (
+    <>
+      <Preview/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div id="navWrapper" className='bg-white flex justify-between w-11/12 p-2 rounded-md px-5'>
+          <button id="backToEditorButton" 
+          className='p-3 border-2 rounded-md border-purply text-base text-purply w-40
+            hover:bg-purply hover:text-white
+          '
+          >Back to Editor
+          </button>
+          <button id="shareLinkButton"
+          className='ml-2 p-3 border-2 rounded-md text-base bg-purply w-40 text-white
+            hover:border-purply hover:bg-white hover:text-purply
+          '>
+                Share Link
+          </button>
+        </div>
+    </>
+  )
+}
+
+export default Navbar

7f83bc2441dfc894e6603d15c9ad417a3b9f2d17
Sumit Kumar Jha <jhasumit4742@gmail.com>
2024-01-29 21:58:44 +0530

    Created the link form using Formik, allowing user to save their dev links

--- src/components/LinkForm.jsx
+++ src/components/LinkForm.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import toast from 'react-hot-toast';
 
-const LinkForm = () => {
+const LinkForm = ({ onLinkSave }) => {
+  const handleSubmit = (values, { resetForm, setSubmitting }) => {
+    // Simple validation
+    if (!values.platform || !values.link) {
+      // Show error toast
+      toast.error('Please fill out all fields.');
+      setSubmitting(false);
+      return;
+    }
+
+    // Notify parent component to save link
+    onLinkSave(values);
+
+    // Show success toast notification
+    toast.success('Link saved successfully!');
+    
+  };
+
   return (
-    <>
-        
-    </>
-  )
-}
+    <div>
+      <Formik
+        initialValues={{ platform: '', link: '' }}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div className="mb-4">
+            <label htmlFor="platform" className="block text-sm font-medium text-gray-700">
+              Platform
+            </label>
+            <Field as="select" name="platform" className="mt-1 p-2 border rounded-md w-full">
+              <option value="" disabled>
+                Select Platform
+              </option>
+              <option value="Github">Github</option>
+              <option value="Linkedin">Linkedin</option>
+              <option value="Leetcode">Leetcode</option>
+              {/* Add more options as needed */}
+            </Field>
+            <ErrorMessage name="platform" component="div" className="text-red-500 text-sm" />
+          </div>
 
-export default LinkForm
+          <div className="mb-4">
+            <label htmlFor="link" className="block text-sm font-medium text-gray-700">
+              Link
+            </label>
+            <Field type="text" name="link" className="mt-1 p-2 border rounded-md w-full" />
+            <ErrorMessage name="link" component="div" className="text-red-500 text-sm" />
+          </div>
+
+          <button type="submit" className="bg-blue-500 text-white p-2 rounded-md">
+            Save
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default LinkForm;
--- src/components/LinkViewAndForm.jsx
+++ src/components/LinkViewAndForm.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import phoneOutline from "../assets/phoneOutline.png"
 import  { Toaster } from 'react-hot-toast'
-import { useSelector } from 'react-redux'
 import LinkForm from './LinkForm'
 
 
+
 const LinkViewAndForm = () => {
-  const userData = useSelector((state)=>state.userData);
+  const [linkData, setLinkData] = useState([]);
+  const [formCount, setFormCount] = useState(1);
+
+  const addNewForm = () => {
+    setFormCount((prevCount) => prevCount + 1);
+  };
+
+  const handleLinkSave = (values) => {
+    setLinkData((prevData) => [...prevData, values]);
+  };
   
   
   return(
     <>
     <Toaster/>
         <div id="container" className='p-2 m-3 flex gap-2 h-screen mb-8'>
-          <div id="mobileWrapper" className='basis-2/5 border-2 h-full  rounded-xl bg-white p-2 m-2 flex justify-center items-center'>
+          <div id="mobileWrapper" className='basis-2/5 border-2 h-full  rounded-xl bg-white p-2 m-2 flex justify-center items-center max-h-[85%]'>
             <div id="profilePreview" className='flex justify-center items-center h-full w-2/3'>
-              <img src={phoneOutline} className='h-[80%] w-[25%] absolute' alt='Phone Outline'/>
+              <img src={phoneOutline} className='h-[70%] w-[20%] absolute' alt='Phone Outline'/>
               <div className="absolute w-[29%] top-60 left-27 text-black">
-                <div id="cardWrapper" className='mt-5'>
+                {/* <div id="cardWrapper" className='mt-5'>
                   <div id="imageContainer" className='w-full flex justify-center'>
                   {userData.profilePhoto && <img src={userData.profilePhoto } alt="avatar" className='h-24 w-24 rounded-full border-4 border-purply'/>}
                   </div>
@@ -25,20 +34,29 @@ const LinkViewAndForm = () => {
                       <h2 className=' text-3xl font-bold text-gray-700'>{userData.firstName} {userData.lastName}</h2>
                       <p className='text-sm text-gray-500'>{userData.email}</p>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
-          <div id="profileDetailsWrapper" className='basis-3/5 border-2 h-full rounded-xl bg-white m-2 '>
+          <div id="profileDetailsWrapper" className='basis-3/5 border-2 h-full rounded-xl bg-white m-2 overflow-y-auto max-h-[85%]'>
             <div id="headingDetails" className='h-30 w-full pt-5 pl-5 pb-2 flex flex-col gap-3'>
               <h1 className='font-bold text-3xl text-gray-800'> Customize your links </h1>
               <p className=' text-base text-gray-600'>Add/edit/remove links below and then share all your profiles with the world!</p>
             </div>
-            <div id="form ">
-              <LinkForm />
+            <div id="form" className="p-5">
+            {[...Array(formCount)].map((_, index) => (
+              <div key={index} className="mb-4">
+                <h2 className="font-bold text-2xl text-gray-800">Link #{index + 1}</h2>
+                <LinkForm onLinkSave={handleLinkSave} />
+              </div>
+            ))}
+            <button onClick={addNewForm} className="bg-green-500 text-white p-2 rounded-md">
+              Add New Link
+            </button>
+          </div>
             </div>
           </div>
-        </div>
+        
         
     </>
   )
